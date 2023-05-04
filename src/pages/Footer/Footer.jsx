47572282bc5f5c1a45a838646df81412623564e3
anikaaurony <aurony111@gmail.com>
2023-05-04 18:28:55 +0600

    footer done   done

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h3>This is Footer</h3>
+        <div className='bg-dark mt-5 text-light p-3'>
+            <div className='d-flex justify-content-around mt-3'>
+                <div>
+                    <h4>Explore More...</h4>
+                    <p>About Us...</p>
+                    <p>Client Reviews...</p>
+                    <p>Food Blogs...</p>
+                </div>
+                <div>
+                    <h5>Contact us:</h5>
+                    <h6>Radhunir Ghar</h6>
+
+                    <p>Address: 20, Bangshal, Dhaka 1210</p>
+                    <p>Mob: 019348235</p>
+
+                </div>
+            </div>
+            <p className='text-center mt-3 '> Copyright © 2020. Radhunir Ghar</p>
         </div>
     );
 };
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -4,7 +4,7 @@ import { useLoaderData } from 'react-router-dom';
 import Chef from '../Chefs/Chef';
 import { Card, Col, Row } from 'react-bootstrap';
 import Popular from './Popular/Popular';
-import Services from '../Services/services';
+
 // import { GrDeliver } from 'react-icons/fa';
 
 const Home = () => {
@@ -30,7 +30,7 @@ const Home = () => {
                 <h3 className='text-success my-5 text-center'>Our Services</h3>
                 <Row xs={1} md={3} className="g-4">
                     <Col>
-                        <Card border="info" style={{ width: '15rem' }}>
+                        <Card border="success" style={{ width: '15rem' }}>
 
                             <Card.Body className='text-center'>
                                 <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGE0p0y9iE7eSnIakLs0BEk36O9AYh44lfHA&usqp=CAU" style={{ height: '100px', width: '100px' }} />
@@ -40,7 +40,7 @@ const Home = () => {
                         </Card>
                     </Col>
                     <Col>
-                        <Card border="info" style={{ width: '15rem' }}>
+                        <Card border="success" style={{ width: '15rem' }}>
 
                             <Card.Body className='text-center'>
                                 <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/1940/1940981.png" style={{ height: '100px', width: '100px' }} />
@@ -51,7 +51,7 @@ const Home = () => {
                         </Card>
                     </Col>
                     <Col>
-                        <Card border="info" style={{ width: '15rem' }}>
+                        <Card border="success" style={{ width: '15rem' }}>
 
                             <Card.Body className='text-center'>
                                 <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2037/2037551.png" style={{ height: '100px', width: '100px' }} />
