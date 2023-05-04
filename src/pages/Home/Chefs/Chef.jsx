34aaf6a9a3712a77d80d53ac6b409547afbafadd
anikaaurony <aurony111@gmail.com>
2023-05-04 23:53:55 +0600

    finally1  done

--- src/pages/Home/Chefs/Chef.jsx
+++ src/pages/Home/Chefs/Chef.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Button, Card, Col } from 'react-bootstrap';
 import LazyLoad from 'react-lazy-load';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Link } from 'react-router-dom';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
-const Chef = ({chef}) => {
-    const {_id, picture, likes, experiance, name, recipesNumber} = chef;
+
+const Chef = ({ chef }) => {
+    const { _id, picture, likes, experiance, name, recipesNumber } = chef;
     return (
         <div>
             <Col >
                 <Card >
-                    <LazyLoad height={262}>
-                    <Card.Img variant="top" className='' style={{height: '250px'}} src={picture}/></LazyLoad>
+                    <LazyLoadImage  effect="blur" variant="top" className='' style={{ height: '250px',  width: '373px'}} src={picture}></LazyLoadImage>
+                    
                     <Card.Body>
                         <Card.Title>{name}</Card.Title>
                         <Card.Text>
@@ -19,7 +22,7 @@ const Chef = ({chef}) => {
                             <div className='d-flex justify-content-between'>
                                 <p>Likes: {likes}</p>
                                 <Link to={`/chefs/${_id}`}><Button variant="outline-success" >View Recipes</Button></Link>
-                                
+
                             </div>
                         </Card.Text>
                     </Card.Body>
