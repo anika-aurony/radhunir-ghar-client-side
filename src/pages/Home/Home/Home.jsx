5e4ede0811024119f7a7a2c0e1479b705690d50a
anikaaurony <aurony111@gmail.com>
2023-05-04 18:10:11 +0600

    extra two   done

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import { useLoaderData } from 'react-router-dom';
 import Chef from '../Chefs/Chef';
-import { Row } from 'react-bootstrap';
+import { Card, Col, Row } from 'react-bootstrap';
 import Popular from './Popular/Popular';
+import Services from '../Services/services';
+// import { GrDeliver } from 'react-icons/fa';
 
 const Home = () => {
 
@@ -22,7 +24,43 @@ const Home = () => {
             </div>
             <div className='mt-3 px-5'>
                 <h3 className='text-success my-5 text-center'>Most Popular Food in Bangladesh</h3>
-                    <Popular></Popular>
+                <Popular></Popular>
+            </div>
+            <div className='mt-3 px-5 ms-5'>
+                <h3 className='text-success my-5 text-center'>Our Services</h3>
+                <Row xs={1} md={3} className="g-4">
+                    <Col>
+                        <Card border="info" style={{ width: '15rem' }}>
+
+                            <Card.Body className='text-center'>
+                                <Card.Img variant="top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGE0p0y9iE7eSnIakLs0BEk36O9AYh44lfHA&usqp=CAU" style={{ height: '100px', width: '100px' }} />
+                                <Card.Title>Home Service</Card.Title>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card border="info" style={{ width: '15rem' }}>
+
+                            <Card.Body className='text-center'>
+                                <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/1940/1940981.png" style={{ height: '100px', width: '100px' }} />
+
+                                <Card.Title>Catering Services</Card.Title>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card border="info" style={{ width: '15rem' }}>
+
+                            <Card.Body className='text-center'>
+                                <Card.Img variant="top" src="https://cdn-icons-png.flaticon.com/512/2037/2037551.png" style={{ height: '100px', width: '100px' }} />
+                                <Card.Title>Event Management</Card.Title>
+                                
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
 
         </div>
---
+++ src/pages/Home/Services/Services.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const Services = () => {
+    return (
+        <div>
+            <Card border="info" style={{ width: '18rem' }}>
+                <Card.Header>Home Delivery</Card.Header>
+                <Card.Body>
+                    <Card.Title>Info Card Title</Card.Title>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </div>
+    );
+};
+
+export default Services;
